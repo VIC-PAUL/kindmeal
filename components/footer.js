b6bb4436a786b75d4566ee1e70e4b988c67d138e
saikhmirsat <saikh.mirsat.786@gmail.com>
2022-08-24 19:25:38 -0700

    home page added by Saikh Mirsat

---
+++ components/footer.js
@@ -0,0 +1,71 @@
+function dishplayFooter() {
+    return ` <div>
+    <div>
+        <div class="fot-head">General</div>
+        <a href="#">Home</a>
+        <a href="#">Sign Up</a>
+        <a href="#">Businesses / Restaurateurs</a>
+        <a href="#">Advertising</a>
+        <a href="#">About KindMeal.my</a>
+        <a href="#">Help & FAQ</a>
+        <a href="#">Terms & Conditions</a>
+        <a href="#">Inspiring Partners</a>
+        <a href="#">Lifestyle Ambassadors</a>
+        <a href="#">Jobs & Careers</a>
+        <a href="#">Contact Us</a>
+    </div>
+    <div>
+        <div class="fot-head">Features</div>
+        <a href="#">Meat-Free Deals</a>
+        <a href="#">Tasty Menus</a>
+        <a href="#">Kind Moments</a>
+        <a href="#">Meat-Free Recipes</a>
+        <a href="#">Member Recommendations</a>
+        <a href="#">Featured Restaurants</a>
+        <a href="#">Vegetarian & Vegan Directory</a>
+        <a href="#">Food Map</a>
+        <a href="#">Become A Superhero</a>
+        <a href="#">Vegan News & Vegetarian Articles</a>
+        <a href="#">Latest Comments</a>
+    </div>
+    <div>
+        <div class="fot-head">Social Media</div>
+        <a href="#">KindMeal Widget</a>
+        <a href="#">Facebook</a>
+        <a href="#">Twitter</a>
+        <a href="#">Instagram</a>
+    </div>
+    <div>
+        <div class="fot-head">Mobile</div>
+        <a href="#">iPhone & iPod App</a>
+        <a href="#">Android App</a>
+        <br>
+        <div class="fot-head">Exciting Promos</div>
+        <a href="#">Gadhimai: Ending Mass Slaughter</a>
+        <a href="#">Free Meals</a>
+        <a href="#">Food Bloggers</a>
+        <a href="#">Uber CHIRP</a>
+        <a href="#">Jane Goodall Contest</a>
+        <a href="#">Win Digi iPhone 6</a>
+        <a href="#">Feed The Poor</a>
+        <a href="#">Win Superhero Gifts</a>
+        <a href="#">Win an iPad Mini 3</a>
+
+    </div>
+    <div>
+        <div class="fot-head">PetFinder.my</div>
+        <a href="#">Adopt A Pet</a>
+        <a href="#">Smartphone Apps</a>
+        <a href="#">WAGazine</a>
+        <a href="#">Discussion Forum</a>
+        <a href="#">Medical Fund</a>
+    </div>
+</div>
+<div id="fot-para">
+    <p>Copyright © KindMeal.my, 2014 - 2022. All rights reserved.</p>
+    <p>This website promotes compassionate, meat-free dining experience. Some food may contain eggs, dairy
+        products or alcohol, please view individual listings for details.</p>
+</div>`
+}
+
+export default dishplayFooter;
