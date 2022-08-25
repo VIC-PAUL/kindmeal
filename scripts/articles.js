9bb3639eecf3f8a5ad38c0c6401f558d41c5406f
amit59684 <amitadhikary59684@outlook.com>
2022-08-25 11:44:58 +0530

    added to articles

---
+++ scripts/articles.js
@@ -0,0 +1,303 @@
+const article_data=[{
+            id:"1",
+            image:"https://www.kindmeal.my/photos/article/20/20037-l.jpg",
+            title:"Creamy Hasselback Potatoes",
+            author:"by KindMeal.my",
+            desc:"Looking for interesting creamy plant-based potatoes recipes?",
+            view:"View Video",
+            date:"23 August 2022"
+         },
+         {
+            id:"2",
+            image:"https://www.kindmeal.my/photos/article/20/20034-l.jpg",
+            title:"Busy Beaver Delivers Lettuce To Zoo Friends",
+            author:"by KindMeal.my",
+            desc:"Filbert the beaver encourages his friends to eat more vege!",
+            view:"View Video",
+            date:"21 August 2022"
+         },
+         {
+            id:"3",
+            image:"https://www.kindmeal.my/photos/article/20/20032-l.jpg",
+            title:"Teaching My Pet Pig To Talk 🐷",
+            author:"by KindMeal.my",
+            desc: "Teaching pet pig to talk 🐷 " ,
+            view:"View Video",
+            date:" 20 August 2022"
+         },
+         {
+            id:"4",
+            image:"https://www.kindmeal.my/photos/article/20/20029-l.jpg",
+            title:"Hatching Animals 🐣",
+            author:"by KindMeal.my",
+            desc:"These little animals have their first experience of the outside world. 🥰",
+            view:"View Video",
+            date:"18 August 2022"
+         },
+         {
+            id:"5",
+            image:"https://www.kindmeal.my/photos/article/20/20036-l.jpg",
+            title:"All Animals Deserve To Be Happy. Let''s Continue To Create A Loving World For Ani..",
+            author:"by Compassion Over Killing",
+            desc:"All animals deserve to be happy. Let's continue to create a loving world for animals everywhere. #animalrights #GoVegan",
+            view:"View Article",
+            date:"22 August 2022"
+         },
+         {
+            id:"6",
+            image:"https://www.kindmeal.my/photos/article/20/20035-l.jpg",
+            title:"Every Person Who Observes Meatless Mondays Reduces Their Carbon Footprint By Eig..",
+            author:"by Compassion Over Killing",
+            desc:"Every person who observes Meatless Mondays reduces their carbon footprint by eight pounds per day - the same impact as not driving for two days - and also saves 28 land and 175 aquatic animals per year. Read more about how eating sustainably can combat #climatechange:",
+            view:"View Article",
+            date:"21 August 2022"
+         },
+         {
+            id:"7",
+            image:"https://www.kindmeal.my/photos/article/20/20031-l.jpg",
+            title:"San Francisco Live Animal Markets Accused Of Cruelty",
+            author:"by Compassion Over Killing",
+            desc:"Survey after survey finds near-unanimous opposition to animal cruelty. There are laws against it, yet it is allowed to continue under the veil of live markets. Our new investigation into Bay Area live markets was recently covered in a story from KRON 4 News. Read more:",
+            view:"View Article",
+            date:"19 August 2022"
+         },
+         {
+            id:"8",
+            image:"https://www.kindmeal.my/photos/article/20/20030-l.jpg",
+            title:"Take Action Today Go Vegan For The Animals. Goveganfortheanimals. Image Jo-anne ..",
+            author:"by Compassion Over Killing",
+            desc:"Take action today: Go vegan for the animals. #GoVeganForTheAnimals Image: Jo-Anne McArthur / @We Animals Media",
+            view:"View Article",
+            date:"18 August 2022"
+         },
+         {
+            id:"9",
+            image:"https://www.kindmeal.my/photos/article/20/20028-l.jpg",
+            title:"In Our New Investigation Of 18 Live Animal Markets In The San Francisco Bay Area..",
+            author:"by Compassion Over Killing",
+            desc:"In our new investigation of 18 live animal markets in the San Francisco Bay Area, we found shocking instances of what we believe to be animal cruelty. While on display for customers, amphibians and reptiles spend their days in plastic tubs with little to no water. Frogs, such as the ones seen here, ..",
+            view:"View Article",
+            date:"17 August 2022"
+         },
+         {
+            id:"10",
+            image:"https://www.kindmeal.my/photos/article/20/20026-l.jpg",
+            title:"Live Animal Markets In San Francisco Accused Of Mistreatment",
+            author:"by Compassion Over Killing",
+            desc:"As National Geographic, which published an article today on our investigation, noted, the goal of Animal Outlook's investigation is to make sure that existing laws are enforced, but also to raise awareness about the exploitation of animals 'no matter if they are fish, frogs, turtles, cows, or pigs..",
+            view:"View Articl",
+            date:"16 August 2022"
+         },
+         {
+            id:"11",
+            image:"https://www.kindmeal.my/photos/article/20/20023-l.jpg",
+            title:"Rescued Cow Asks For More Scratches",
+            author:"by KindMeal.my",
+            desc:"❤",
+            view:"View Video",
+            date:"15 August 2022"
+         },
+         {
+            id:"12",
+            image:"https://www.kindmeal.my/photos/article/20/20020-l.jpg",
+            title:"Happy Donkey Plays With His Ball",
+            author:"by KindMeal.my",
+            desc:"What happens when you give a ball to a donkey? 😂",
+            view:"View Video",
+            date:"14 August 2022"
+         },
+         {
+            id:"13",
+            image:"https://www.kindmeal.my/photos/article/20/20017-l.jpg",
+            title:"Rescuing Trapped Animals 😥",
+            author:"by KindMeal.my",
+            desc:"Amazing people rescuing trapped animals. 💪",
+            view:"View Video",
+            date:"13 August 2022"
+         },
+         {
+            id:"14",
+            image:"https://www.kindmeal.my/photos/article/20/20027-l.jpg",
+            title:"Read National Geographic Latest Article Covering Our New Investigation Into Live..",
+            author:"by Compassion Over Killing",
+            desc:"Read National Geographic latest article covering our new investigation into Live Markets located in the Bay Area:",
+            view:"View Article",
+            date:"16 August 2022"
+         },
+         {
+            id:"15",
+            image:"https://www.kindmeal.my/photos/article/20/20025-l.jpg",
+            title:"Breaking Investigation We Recently Investigated 18 Live Animal Markets In The Sa..",
+            author:"by Compassion Over Killing",
+            desc:"🚨BREAKING INVESTIGATION🚨: We recently investigated 18 live animal markets in the San Francisco Bay Area, where we documented many instances of what we believe to be animal cruelty. In these live markets, turtles and frogs are kept in plastic containers with no water and are sliced up alive. Fish a..",
+            view:"View Video",
+            date:"16 August 2022"
+         },
+         {
+            id:"16",
+            image:"https://www.kindmeal.my/photos/article/20/20024-l.jpg",
+            title:"Studies Have Shown That Plant-based Foods Have Much Less Of A Negative Climate I..",
+            author:"by Compassion Over Killing",
+            desc:"Studies have shown that plant-based foods have much less of a negative climate impact compared to meat and animal articless. Animal agriculture is both unethical and unsustainable. #goveganfortheplanet #goveganfortheanimals Learn more about the way animal articless negatively impact the planet:",
+            view:"View Article",
+            date:"15 August 2022"
+         },
+         {
+            id:"17",
+            image:"https://www.kindmeal.my/photos/article/20/20021-l.jpg",
+            title:"Show Animals Kindness And Compassion. Animals Are Sentient Beings And Deserve To..",
+            author:"by Compassion Over Killing",
+            desc:"Show animals kindness and compassion. Animals are sentient beings and deserve to live happy and healthy lives. Image: Jo-Anne McArthur / We Animals Media",
+            view:"View Article",
+            date:"14 August 2022"
+         },
+         {
+            id:"18",
+            image:"https://www.kindmeal.my/photos/article/20/20019-l.jpg",
+            title:"Animal Outlook Is Proud To Be A Sponsor Of The Fifth Annual Black Vegfest Which ..",
+            author:"by Compassion Over Killing",
+            desc:"Animal Outlook is proud to be a sponsor of the fifth annual Black VegFest which is taking place on Saturday August 13th, from 12 PM - 8 PM at Commodore Barry Park in Brooklyn, NY. Our own Deputy Director of Investigations, Erin Wing, will be in attendance and speaking as well, joining two other ama..",
+            view:"View Article",
+            date:"13 August 2022"
+         },
+         {
+            id:"19",
+            image:"https://www.kindmeal.my/photos/article/20/20018-l.jpg",
+            title:"Animals Are Sentient Beings, Not Just Numbers, Which Is How The Animal Agricultu..",
+            author:"by Compassion Over Killing",
+            desc:"Animals are sentient beings, not just numbers, which is how the animal agriculture industry considers them. #GoVegan and make a difference.",
+            view:"View Article",
+            date:"13 August 2022"
+         },
+         {
+            id:"20",
+            image:"https://www.kindmeal.my/photos/article/20/20016-l.jpg",
+            title:"All Animals Deserve To Be Free, Happy, And Healthy. Govegan For The Animals. Ima..",
+            author:"by Compassion Over Killing",
+            desc:"All animals deserve to be free, happy, and healthy. #GoVegan for the animals. Image: Lukas Vincour / Zvířata Nejíme / We Animals Media",
+            view:"View Article",
+            date:"12 August 2022"
+         },
+        
+
+]
+localStorage.setItem("racial_class",JSON.stringify(article_data));
+
+const LSData=JSON.parse(localStorage.getItem("racial_class"));
+
+const main=document.querySelector(".main");
+function display(data=[]){
+    main.innerHTML=null;
+ data.forEach(function(element){
+  const main_div= document.createElement("div");
+  main_div.id=("main_div")
+  const img_div= document.createElement("div");
+  img_div.id=("img_div")
+  const text_div=document.createElement("div");
+  text_div.id=("text_div")
+//   const bottom_div=document.createElement("div");
+//   ext_div.id=("text_div")
+
+  const image=document.createElement("img");
+  image.src=element.image;
+
+  const title=document.createElement("h2");
+   
+  title.textContent = element.title;
+
+  const author=document.createElement("p");
+  author.textContent = element.author;
+
+  const desc=document.createElement("p");
+  desc.textContent=element.desc;
+  const bottom_div=document.createElement("div");
+  bottom_div.id=("bottom_div")
+
+
+  const date=document.createElement("tr");
+  date.textContent = element.date;
+
+  const view=document.createElement("button");
+  view.id=("view_text")
+  view.textContent=element.view;
+  view.addEventListener("click",function(View_your){
+   View_your=image.innerHTML
+    View_your(element);
+    
+  })
+   img_div.append(image)
+   text_div.append(title,author,desc,bottom_div);
+   bottom_div.append(view,date)
+   main_div.append(img_div,text_div);
+   main.append(main_div);
+ })
+}
+display(LSData);
+
+
+
+
+// // selecting required element
+// const element = document.querySelector(".pagination ul");
+// let totalPages = 30;
+// let page = 1;
+
+// //calling function with passing parameters and adding inside element which is ul tag
+// element.innerHTML = createPagination(totalPages, page);
+// function createPagination(totalPages, page){
+//   let liTag = '';
+//   let active;
+//   let beforePage = page - 1;
+//   let afterPage = page + 1;
+//   if(page > 1){ //show the next button if the page value is greater than 1
+//     liTag += `<li class="btn prev" onclick="createPagination(totalPages, ${page - 1})"><span><i class="fas fa-angle-left"></i> Prev</span></li>`;
+//   }
+
+//   if(page > 2){ //if page value is less than 2 then add 1 after the previous button
+//     liTag += `<li class="first numb" onclick="createPagination(totalPages, 1)"><span>1</span></li>`;
+//     if(page > 3){ //if page value is greater than 3 then add this (...) after the first li or page
+//       liTag += `<li class="dots"><span>...</span></li>`;
+//     }
+//   }
+
+//   // how many pages or li show before the current li
+//   if (page == totalPages) {
+//     beforePage = beforePage - 2;
+//   } else if (page == totalPages - 1) {
+//     beforePage = beforePage - 1;
+//   }
+//   // how many pages or li show after the current li
+//   if (page == 1) {
+//     afterPage = afterPage + 2;
+//   } else if (page == 2) {
+//     afterPage  = afterPage + 1;
+//   }
+
+//   for (var plength = beforePage; plength <= afterPage; plength++) {
+//     if (plength > totalPages) { //if plength is greater than totalPage length then continue
+//       continue;
+//     }
+//     if (plength == 0) { //if plength is 0 than add +1 in plength value
+//       plength = plength + 1;
+//     }
+//     if(page == plength){ //if page is equal to plength than assign active string in the active variable
+//       active = "active";
+//     }else{ //else leave empty to the active variable
+//       active = "";
+//     }
+//     liTag += `<li class="numb ${active}" onclick="createPagination(totalPages, ${plength})"><span>${plength}</span></li>`;
+//   }
+
+//   if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
+//     if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+//       liTag += `<li class="dots"><span>...</span></li>`;
+//     }
+//     liTag += `<li class="last numb" onclick="createPagination(totalPages, ${totalPages})"><span>${totalPages}</span></li>`;
+//   }
+
+//   if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
+//     liTag += `<li class="btn next" onclick="createPagination(totalPages, ${page + 1})"><span>Next <i class="fas fa-angle-right"></i></span></li>`;
+//   }
+//   element.innerHTML = liTag; //add li tag inside ul tag
+//   return liTag; //reurn the li tag
+// }
