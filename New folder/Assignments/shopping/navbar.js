848d729be98b1f97ed0c5d28faa243924fb48fdb
Amit Adhikary <107456534+amit59684@users.noreply.github.com>
2022-08-27 16:01:39 +0530

    Delete New folder directory

--- New folder/Assignments/shopping/navbar.js
+++
@@ -1,6 +0,0 @@
-const user= document.querySelector(".user");
-    const signinData=JSON.parse(localStorage.getItem("signin"));
-    if(signinData != null){
-       user.innerHTML=`<h2>  ${signinData.username.toUpperCase()} </h2>
-            <h2> <a href="./cart.html"> Cart </a></h2>`;
-    }
